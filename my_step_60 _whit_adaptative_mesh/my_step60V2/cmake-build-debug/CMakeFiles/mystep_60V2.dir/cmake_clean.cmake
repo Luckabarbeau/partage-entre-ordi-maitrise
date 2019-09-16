2a372c009beb43b4bb78a8da9d8d66f9aae6fe9b
Lucka <luckabarbeau@gmail.com>
2019-09-16 13:50:03 -0400

    working whit adaptastive mesh

---
+++ my_step_60 _whit_adaptative_mesh/my_step60V2/cmake-build-debug/CMakeFiles/mystep_60V2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mystep_60V2.dir/mystep_60V2.cpp.o"
+  "mystep_60V2.pdb"
+  "mystep_60V2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mystep_60V2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
